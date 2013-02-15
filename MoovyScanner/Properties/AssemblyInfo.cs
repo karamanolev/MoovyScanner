dfbc6b67969c2decdf76bc5425f9d20bdd2a068e
Ivailo Karamanolev <ivailo@karamanolev.com>
2013-02-15 15:02:04 +0200

    MoovyCopy: icon. MoovyScanner: info and bug fix.

--- MoovyScanner/Properties/AssemblyInfo.cs
+++ MoovyScanner/Properties/AssemblyInfo.cs
@@ -1,15 +1,14 @@
 ﻿using System.Reflection;
-using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("ImdbIdExtractor")]
+[assembly: AssemblyTitle("MoovyScanner")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Microsoft")]
-[assembly: AssemblyProduct("ImdbIdExtractor")]
+[assembly: AssemblyProduct("MoovyScanner")]
 [assembly: AssemblyCopyright("Copyright © Microsoft 2012")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
